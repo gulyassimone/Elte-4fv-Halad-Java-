f1e43222f6f2f59dce41250f3d4a09df3d36d05c
gulyassimone <gulyassimone@gmail.com>
2021-02-12 12:37:11 +0000

    1. lessons

--- 1. GY/src/test/java/hu/elte/haladojava/matrix/MatrixTest.java
+++
@@ -1,7 +0,0 @@
-package hu.elte.haladojava.matrix;
-
-public class MatrixTest {
-
-  // TODO
-
-}
---
+++ gyakorlat1/withoutmvn/hu/elte/haladojava/matrix/MatrixTest.java
@@ -0,0 +1,12 @@
+package hu.elte.haladojava.matrix;
+
+import org.junit.Test;
+
+public class MatrixTest {
+
+  @Test
+    public void test(){
+
+  }
+
+}
